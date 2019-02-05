0e03dbb01ee39dd320a2e1a421e2772de115eba5
Thomas Diers <thomasd.handifablabpro@gmail.com>
2019-02-05 14:40:52 +0100

    My first commit

---
+++ 01-bases/index.php
@@ -0,0 +1,20 @@
+
+
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <title>Page Title</title>
+</head>
+<body>
+
+    <?php
+
+    // Ici, je peux écrire mon code PHP
+    echo '<h1>L\'titre simple quote<h1>';
+    echo "<h1>L'titre double quotes<h1>";
+
+    ?>
+
+</body>
+</html>
+
---
+++ 02-variables/hello.php
@@ -0,0 +1,7 @@
+<?php
+
+$firestName = 'Thomas';
+
+echo '<h1>Bonjour ' .$prenom. ' !</h1>';
+// Interpolation de variable possible avec les doubles quotes
+echo "<h1>Bonjour $firestName</h1>";
