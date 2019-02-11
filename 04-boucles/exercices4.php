24be78f6c7ddf150328ccb393d2ea7b9005970f6
Thomas Diers <thomasd.handifablabpro@gmail.com>
2019-02-11 17:03:46 +0100

    bla

--- 04-boucles/exercices4.php
+++ 04-boucles/exercices4.php
@@ -5,3 +5,61 @@ echo ', il est ';
 echo date ('H\hi \e\t ');
 echo $s . ' seconde';
 echo ($s > 1) ? 's' : '';
+
+
+
+
+
+echo "</br>"; 
+echo "</br>"; 
+echo "</br>"; 
+
+
+
+
+
+
+$eleves = [
+    0 => [
+        'nom' => 'Matthieu',
+        'notes' => [10, 8, 16, 20, 17, 16, 15, 2]
+    ],
+    1 => [
+        'nom' => 'Thomas',
+        'notes' => [4, 18, 12, 15, 13, 7]
+    ],
+    2 => [
+        'nom' => 'Jean',
+        'notes' => [17, 14, 6, 2, 16, 18, 9]
+    ],
+    3 => [
+        'nom' => 'Enzo',
+        'notes' => [1, 14, 6, 2, 1, 8, 9]
+    ]
+];
+echo "</br>";
+// Exemple pour parcourir un tableau
+foreach ($eleves as $eleve) {
+    echo $eleve['nom'];
+    echo " a eu ";
+    foreach ($eleve['notes'] as $note) {
+        echo $note . ', ';
+        echo $str =strlen();
+    };
+    echo "</br>"; 
+    echo "</br>"; 
+};
+
+
+
+
+
+
+
+echo "</br>"; 
+echo "</br>"; 
+echo "</br>";
+print_r($eleves[0]['notes']);
+
+
+/*  */
